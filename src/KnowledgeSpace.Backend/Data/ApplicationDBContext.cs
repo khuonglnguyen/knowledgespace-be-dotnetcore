db59f2803e201961642bfd4eb3d358482aecaf97
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-01-15 11:05:38 +0700

    Config ApplicationDbContext and Fluent API

---
+++ src/KnowledgeSpace.Backend/Data/ApplicationDBContext.cs
@@ -0,0 +1,53 @@
+﻿using KnowledgeSpace.Backend.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace KnowledgeSpace.Backend.Data
+{
+    public class ApplicationDBContext : IdentityDbContext<User>
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().Property(x => x.Id).HasMaxLength(50).IsUnicode(false);
+            builder.Entity<User>().Property(x => x.Id).HasMaxLength(50).IsUnicode(false);
+
+            builder.Entity<LabelInKnowledgeBase>()
+                        .HasKey(c => new { c.LabelId, c.KnowledgeBaseId });
+
+            builder.Entity<Permission>()
+                       .HasKey(c => new { c.RoleId, c.FunctionId, c.CommandId });
+
+            builder.Entity<Vote>()
+                        .HasKey(c => new { c.KnowledgeBaseId, c.UserId });
+
+            builder.Entity<CommandInFunction>()
+                       .HasKey(c => new { c.CommandId, c.FunctionId });
+
+            builder.HasSequence("KnowledgeBaseSequence");
+        }
+
+        public DbSet<Command> Commands { set; get; }
+        public DbSet<CommandInFunction> CommandInFunctions { set; get; }
+
+        public DbSet<ActivityLog> ActivityLogs { set; get; }
+        public DbSet<Category> Categories { set; get; }
+        public DbSet<Comment> Comments { set; get; }
+        public DbSet<Function> Functions { set; get; }
+        public DbSet<KnowledgeBase> KnowledgeBases { set; get; }
+        public DbSet<Label> Labels { set; get; }
+        public DbSet<LabelInKnowledgeBase> LabelInKnowledgeBases { set; get; }
+        public DbSet<Permission> Permissions { set; get; }
+        public DbSet<Report> Reports { set; get; }
+        public DbSet<Vote> Votes { set; get; }
+
+        public DbSet<Attachment> Attachments { get; set; }
+    }
+}
