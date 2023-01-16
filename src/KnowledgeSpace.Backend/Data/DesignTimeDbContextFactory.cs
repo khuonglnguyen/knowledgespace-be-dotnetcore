755839e16709ae9f929c63f3bceb6dc19a8d2ad7
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-01-16 20:11:19 +0700

    Migration DB

---
+++ src/KnowledgeSpace.Backend/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
+
+namespace KnowledgeSpace.Backend.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDBContext>
+    {
+        public ApplicationDBContext CreateDbContext(string[] args)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{environmentName}.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<ApplicationDBContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+            return new ApplicationDBContext(builder.Options);
+        }
+    }
+}
