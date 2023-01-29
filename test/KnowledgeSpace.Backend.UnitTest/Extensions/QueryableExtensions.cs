4431f01c14d587eab935c20aca47efa2f985e6c8
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-01-29 20:16:13 +0700

    Add unit test RolesController - Get Role Failed

---
+++ test/KnowledgeSpace.Backend.UnitTest/Extensions/QueryableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using KnowledgeSpace.Backend.UnitTest.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KnowledgeSpace.Backend.UnitTest.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> AsAsyncQueryable<T>(this IEnumerable<T> input)
+        {
+            return new NotInDbSet<T>(input);
+        }
+    }
+}
