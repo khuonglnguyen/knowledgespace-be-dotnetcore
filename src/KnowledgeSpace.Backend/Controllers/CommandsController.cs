09207bb8d20988cec55398ac2a7a273c6ea86ce9
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-03-13 18:25:05 +0700

    Add command controller

---
+++ src/KnowledgeSpace.Backend/Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using KnowledgeSpace.Backend.Data;
+using KnowledgeSpace.ViewModels.Systems;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnowledgeSpace.Backend.Controllers
+{
+    public class CommandsController : BaseController
+    {
+        private readonly ApplicationDBContext _context;
+
+        public CommandsController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetCommants()
+        {
+            var commands = _context.Commands;
+
+            var commandVms = await commands.Select(u => new CommandVm()
+            {
+                Id = u.Id,
+                Name = u.Name,
+            }).ToListAsync();
+
+            return Ok(commandVms);
+        }
+    }
+}
