de5775ac5e57ae317f7d4d81f7cb488dc2acf45e
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-04-10 19:42:00 +0700

    Add Categories Controller

---
+++ src/KnowledgeSpace.Backend/Services/SequenceService.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace KnowledgeSpace.Backend.Services
+{
+    public class SequenceService : ISequenceService
+    {
+        private readonly IConfiguration _configuration;
+
+        public SequenceService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<int> GetKnowledgeBaseNewId()
+        {
+            using (SqlConnection conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    await conn.OpenAsync();
+                }
+
+                var result = await conn.ExecuteScalarAsync<int>(@"SELECT (NEXT VALUE FOR KnowledgeBaseSequence)", null, null, 120, CommandType.Text);
+                return result;
+            }
+        }
+    }
+}
